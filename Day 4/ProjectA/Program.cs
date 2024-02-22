bed323b21164d0badf3bae1bab3e116a885c77cd
yudharisandy <yudhapa17@gmail.com>
2024-02-22 08:29:28 +0700

    Update Day 4 Project

---
+++ Day 4/ProjectA/Program.cs
@@ -0,0 +1,20 @@
+﻿// Interface
+class Program
+{
+    static void Main(){
+
+    }
+}
+
+public interface class Animal
+{
+    public interface void MakeSound()
+    {
+
+    }
+}
+
+public class Cat : Animal
+{
+    
+}
